8e5a4b9e2f68340df1d318993284ee6a0e01f4c3
Amam <amam11rachmanto@gmail.com>
2023-12-19 13:54:54 +0700

    1 init git hub

---
+++ src/components/CoinItem/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Text, View, Image } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import styles from "./styles";
+
+const CoinItem = ({ marketCoin }) => {
+    const { name, current_price, market_cap_rank, price_change_percentage_24h, symbol, market_cap, image, } = marketCoin;
+
+    const percentageColor = price_change_percentage_24h < 0 ? '#ea3943' : '#16c784'
+
+    const normallizeMarketCap = (marketcap) => {
+        if (marketcap > 1_000_000_000_000) {
+            return `${Math.floor(marketcap / 1_000_000_000_000)} T`;
+        } else if (marketcap > 1_000_000_000) {
+            return `${Math.floor(marketcap / 1_000_000_000)} B`;
+        } else if (marketcap > 1_000_000) {
+            return `${Math.floor(marketcap / 1_000_000)} M`;
+        } else if (marketcap > 1_000) {
+            return `${Math.floor(marketcap / 1_000)} K`;
+        }
+        return marketcap;
+    };
+    
+
+    return (
+        <View style={styles.coinContainer}>
+        <Image 
+        source={{uri: image}} 
+        style={{height: 30, width: 30, marginRight: 10, alignSelf: 'center'}}
+        />
+        <View>
+            <Text style={styles.tittle}>{name}</Text>
+            <View style={{flexDirection: 'row'}}>
+            <View style={styles.rankContainer}>
+                <Text style={styles.rank}>{market_cap_rank}</Text>
+            </View>
+            <Text style={styles.text}>{symbol.toUpperCase()}</Text>
+            <AntDesign name={price_change_percentage_24h < 0 ? 'caretdown' : 'caretup'} size={15} color={percentageColor} style={{alignSelf: 'center', marginRight: 5}}/>
+            <Text style={{color: percentageColor}}>{price_change_percentage_24h.toFixed(2)}%</Text>
+            </View>
+        </View>
+        <View style={{marginLeft: 'auto', alignItems: 'flex-end'}}>
+            <Text style={styles.tittle}>{current_price}</Text>
+            <Text style={{color: 'white'}}>MCap {normallizeMarketCap(market_cap)}</Text>
+        </View>
+        </View>
+    );
+};
+
+export default CoinItem;
